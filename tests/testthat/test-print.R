0b036c22d90436827f02a98eab6c15bdd567d86f
Christof Lewerenz <christof.lewerenz@gmx.net>
2023-09-25 18:24:59 +0200

    printing functions for label attributes

--- tests/testthat/test-print.R
+++ tests/testthat/test-print.R
@@ -2,7 +2,9 @@ test_that(
   "Test print generic", {
 
     x <- i_labelled(iris$Species)
-   expect_output(print.i_labelled(x))
+    expect_output(print.i_labelled(x))
+
+    expect_output(i_print_labels(x))
 
   }
 )
