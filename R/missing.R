0c98b62353c40524c43bf02093d2ea99eafaf173
Christof Lewerenz <christof.lewerenz@gmx.net>
2023-09-29 20:47:54 +0200

    tests missing_to_na

--- R/missing.R
+++ R/missing.R
@@ -65,7 +65,7 @@ i_na_range.data.frame <- function(x, values){
 
 #' missing values to NA
 #' @description
-#' alle values declared as missing will be recoded as NA
+#' all values declared as missing will be recoded as NA
 #' set missing values via i_labelled(), i_na_values() or i_na_range()
 #'
 #' @param x vector or data.frame
