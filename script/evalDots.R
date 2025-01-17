43964c1297f40a34bd37d0ca0202dfc82a6392c0
clewerenz <143947257+clewerenz@users.noreply.github.com>
2025-01-17 15:35:16 +0100

    Update evalDots.R

--- script/evalDots.R
+++ script/evalDots.R
@@ -1,1 +1,9 @@
+evalDotsLabels <- function(...){
+  input <- unlist(list(...))
+  eval(input)
+}
 
+evalDotsLabels(setNames(1:3, LETTERS[1:3]))
+evalDotsLabels(A = 1, B = 2, C = 3)
+evalDotsLabels(c(A = 1, B = 2, C = 3))
+evalDotsLabels(list(A = 1, B = 2, C = 3))
